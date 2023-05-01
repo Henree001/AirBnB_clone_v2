f30aa50d7718691bbb0d6ac4c44d589fdd508b0d
Henree001 <okaforhenry779@gmail.com>
2023-05-01 18:12:56 +0100

    fixed task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,5 +10,26 @@ echo "My new website" | sudo tee /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "15i\    location /hbnb_static {\n\talias /data/web_static/current/;\n\ttry_files \$uri \$uri/ =404;\n\    }" /etc/nginx/sites-available/default
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By \$HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
 sudo service nginx restart
