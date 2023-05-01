3633f3ffcc355883c565dc58c69aa8cdf16358ac
Henree001 <okaforhenry779@gmail.com>
2023-05-01 15:40:34 +0100

    fixed task 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 echo "My new website" | sudo tee /data/web_static/releases/test/index.html
 sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "15i\    location /hbnb_static {\n\talias /data/web_static/current/;\n\    }" /etc/nginx/sites-available/default
+sudo sed -i "15i\    location /hbnb_static/ {\n\talias /data/web_static/current/;\n\ttry_files \$uri \$uri/ =404;\n\    }" /etc/nginx/sites-available/default
 sudo service nginx restart
