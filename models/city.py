d4ddf8a33c04ec3de47afc0574c193700be6c287
Henree001 <okaforhenry779@gmail.com>
2023-05-24 17:33:04 +0100

    fixed task 2

--- models/city.py
+++ models/city.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-import os
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-
-from models.base_model import BaseModel, Base
+"""This module contains information on state"""
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    state_id = Column(
-        String(60), ForeignKey('states.id'), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-    places = relationship(
-        'Place',
-        cascade='all, delete, delete-orphan',
-        backref='cities'
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else None
+class City(BaseModel):
+    """Child class city"""
+    state_id = ""
+    name = ""
