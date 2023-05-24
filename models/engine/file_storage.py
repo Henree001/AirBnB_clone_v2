9743585a905d318d9ce4c7f999e77a6a2bc8682f
Henree001 <okaforhenry779@gmail.com>
2023-05-24 16:23:27 +0100

    fixed place_amenity table not defined

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -58,7 +58,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete object provided"""
-		if obj is not None:
+        if obj is not None:
             obj_key = obj.to_dict()['__class__'] + '.' + obj.id
             if obj_key in self.__objects.keys():
                 del self.__objects[obj_key]
