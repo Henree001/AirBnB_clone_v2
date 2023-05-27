54817de73736125dbe3a2f1a5b5d616b2b8aae58
Henree001 <okaforhenry779@gmail.com>
2023-05-27 10:43:34 +0100

    fixed task 10

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,7 +64,6 @@ class DBStorage:
     def reload(self):
         """create all tables in the database and
         create the current database session"""
-        #Base.metadata.drop_all(self.__engine)
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
